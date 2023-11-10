7bdb3cf0db155d13b4f37043f135aec3cb48e2e8
adanni3 <148531920+adanni3@users.noreply.github.com>
2023-11-10 11:36:34 +0100

    Add files via upload

---
+++ littlepie.py
@@ -0,0 +1,21 @@
+i = 1
+j=2
+k = i*j
+print(k)
+
+for l in range (2):
+    print(k)
+
+if (i < 7):
+    i + 3
+    print(i)
+
+a=[1,2,3]
+print(a)
+print(a[1])
+a.append([4,5])
+print(a)
+print(a[3])
+
+import matportlib as mp
+import numpy as np
